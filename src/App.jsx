f420e88852426a1a5a8d6b30f6504fe3958dfb5e
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-03-01 23:53:26 +0530

    StudyNotion - Vite + React

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
+import PrivateRoute from './Components/PrivateRoute';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
@@ -16,7 +17,11 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}/>} />
         <Route path="/signup" element={<Signup setIsLoggedIn={setIsLoggedIn}/>} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={
+          <PrivateRoute isLoggedIn={isLoggedIn}>
+            <Dashboard />
+          </PrivateRoute>
+        } />
       </Routes>
     </div>
   )
